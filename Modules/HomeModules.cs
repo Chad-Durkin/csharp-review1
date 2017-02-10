660bf69b9516b6d11504ac74dd6b24a305199221
Chad Durkin <Chaddurkin@gmail.com>
2017-02-10 08:56:40 -0800

    add contact info form

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -10,6 +10,10 @@ namespace AddressBookApp
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
+      Get["/contact/form"] = _ => {
+        return View["contact_new.cshtml"];
+      }
+      Post["/contact/new"]
 
     }
   }
